bdc92b3657e90d8f6034fce9ca22727f4c1b7f32
c-botz <168880500+c-botz@users.noreply.github.com>
2024-07-21 10:00:25 -0700

    Delete firmware/qmk/qmk_firmware/keyboards/handwired/c_botz/crowkb_46 directory

--- firmware/qmk/qmk_firmware/keyboards/handwired/c_botz/crowkb_46/rules.mk
+++
@@ -1,4 +0,0 @@
-# This file intentionally left blank
-SPLIT_KEYBOARD = yes        # Use shared split_common code
-LTO_ENABLE = yes            # Use link time optimization for smaller firmware
-OLED_ENABLE = yes

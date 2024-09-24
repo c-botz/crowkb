1ec4ed0903e61c8f09721ab2dd6bfdb6d6f54b81
c-botz <168880500+c-botz@users.noreply.github.com>
2024-09-24 16:07:08 -0700

    Delete firmware/38/qmk/qmk directory

--- firmware/38/qmk/qmk/keyboards/handwired/c_botz/crowkb_38/rules.mk
+++
@@ -1,7 +0,0 @@
-# This file intentionally left blank
-SPLIT_KEYBOARD = yes        # Use shared split_common code
-LTO_ENABLE = yes            # Use link time optimization for smaller firmware
-OLED_ENABLE = yes
-ENCODER_ENABLE = yes
-ENCODER_MAP_ENABLE = yes
-TAP_DANCE_ENABLE = yes

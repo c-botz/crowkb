356611dd7aca3396164c2b8f6543f2df4d1b61ee
c-botz <168880500+c-botz@users.noreply.github.com>
2024-09-24 16:08:13 -0700

    Add files via upload

---
+++ firmware/38/qmk/qmk_firmware/keyboards/handwired/c_botz/crowkb_38/keymaps/vial/config.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x14, 0x09, 0x0C, 0xE5, 0x31, 0x0B, 0x71, 0x77}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 3, 7 }
+#define VIAL_UNLOCK_COMBO_COLS { 4, 4 }
---
+++ firmware/38/qmk/qmk_firmware/keyboards/handwired/c_botz/crowkb_38/rules.mk
@@ -0,0 +1,7 @@
+# This file intentionally left blank
+SPLIT_KEYBOARD = yes        # Use shared split_common code
+LTO_ENABLE = yes            # Use link time optimization for smaller firmware
+OLED_ENABLE = yes
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+TAP_DANCE_ENABLE = yes
